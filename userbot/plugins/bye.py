a84116d311a6dacfb1e8401afdbfc8b396aa7729
tow3rr <69804065+tow3rr@users.noreply.github.com>
2020-09-12 17:41:54 +0200

    Update bye.py

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -3,11 +3,10 @@
 .bye
 """
 from telethon.tl.functions.channels import LeaveChannelRequest
-from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.utils import admin_cmd, edit_or_reply
 import time
 
 @borg.on(admin_cmd("bye", outgoing=True))
-@borg.on(sudo_cmd("bye", allow_sudo=True))
 async def leave(e):
     starkgang = await edit_or_reply(e, "Addio coglioni")
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
